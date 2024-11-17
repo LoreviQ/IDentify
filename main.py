da24c21183dbdaa5bc0524fba2bef9a1887f3c67
LoreviQ <lorevi@protonmail.com>
2024-11-17 13:40:08 +0000

    Added basic code to get transactionsignatures from inputted address

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from solana.rpc.api import Client
+from solders.pubkey import Pubkey 
+
+solana_client = Client("https://api.mainnet-beta.solana.com")
+
+def get_transactions(wallet_address):
+    # Fetch confirmed transactions for the wallet
+    pubkey = Pubkey.from_string(wallet_address)
+    response = solana_client.get_signatures_for_address(pubkey)
+    return response
+
 def main():
-    print("Hello World!")
+    test_adress = "3GNJ8zxnqoe5FPncoqRe2gzAkvopmK7a8NzL2P5DF6yk"
+    transactions = get_transactions(test_adress)
+    print(transactions)
 
 if __name__ == "__main__":
     main()
-
-    """TEST"""
